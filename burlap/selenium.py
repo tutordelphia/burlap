68179b2b2f6dedd995d8b30c9efa98bbd61fee5d
Chris Spencer <chrisspen@gmail.com>
2017-03-05 22:17:02 -0500

    Added selenium test.

--- burlap/selenium.py
+++ burlap/selenium.py
@@ -15,9 +15,9 @@ class SeleniumSatchel(Satchel):
     
     http://www.seleniumhq.org/
     """
-    
+
     name = 'selenium'
-    
+
     def set_defaults(self):        
         # See https://github.com/mozilla/geckodriver/releases for other versions and architectures.
         self.env.geckodriver_version = '0.13.0'
@@ -27,21 +27,31 @@ class SeleniumSatchel(Satchel):
             'v{geckodriver_version}/geckodriver-v{geckodriver_version}-{geckodriver_arch}.tar.gz'
         self.env.geckodriver_install_bin_path = '/usr/local/bin'
         self.env.geckodriver_bin_name = 'geckodriver'
-    
+
+    @property
+    def geckodriver_path(self):
+        r = self.local_renderer
+        return r.format('{geckodriver_install_bin_path}/{geckodriver_bin_name}')
+
     @task
     def install_geckodriver(self):
         r = self.local_renderer
         r.run(
             'cd /tmp; '
-            'wget --show-progress -O geckodriver.tar.gz {geckodriver_url_template}'
+            'wget --show-progress -O geckodriver.tar.gz {geckodriver_url_template}; '
             'tar -xvzf geckodriver.tar.gz')
-        r.sudo('mv /tmp/{geckodriver_bin_name} {geckodriver_install_bin_path}') 
-    
+        r.sudo('mv /tmp/{geckodriver_bin_name} {geckodriver_install_bin_path}')
+
+    @task
+    def uninstall_geckodriver(self):
+        r = self.local_renderer
+        r.sudo('rm -f {geckodriver_install_bin_path}/{geckodriver_bin_name}')
+
     @task(precursors=['packager'])
     def configure(self):
         if self.env.enabled:
             self.install_geckodriver()
         else:
-            self.sudo('rm -f {geckodriver_install_bin_path}/{geckodriver_bin_name}')
-    
+            self.uninstall_geckodriver()
+
 selenium = SeleniumSatchel()
