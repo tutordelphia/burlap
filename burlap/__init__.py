785fa1c510a1821aa65b9174f716f19a3e0cef17
Chris Spencer <chrisspen@gmail.com>
2017-04-22 17:38:47 -0400

    Fixed formatting.

--- burlap/__init__.py
+++ burlap/__init__.py
@@ -69,7 +69,7 @@ except (ImportError, NameError) as e:
     print('Unable to initialize debug: %s' % e, file=sys.stderr)
     debug = None
 
-VERSION = (0, 9, 24)
+VERSION = (0, 9, 25)
 __version__ = '.'.join(map(str, VERSION))
 
 burlap_populate_stack = int(os.environ.get('BURLAP_POPULATE_STACK', 1))
